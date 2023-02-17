b156e27d3aee62c44939804b6be8e5273ffc2638
OlegAnisimov <gramafon@yandex.ru>
2023-02-17 13:22:46 +0300

    structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductEditComponent } from './products/product-edit/product-edit.component';
+import { ProductListComponent } from './products/product-list/product-list.component';
+import { ProductShelComponent } from './products/product-shel/product-shel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductEditComponent,
+    ProductListComponent,
+    ProductShelComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/products/product-shel/product-shel.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-shel',
+  templateUrl: './product-shel.component.html',
+  styleUrls: ['./product-shel.component.scss']
+})
+export class ProductShelComponent {
+
+}
